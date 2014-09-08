50dd49b56958227dff068ffdc28b8c0766592304
Jason Hobbs <grimmstede@feemon.com>
2014-09-08 13:21:57 -0500

    fixed gritter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,6 @@ $(document).ready(
       $.extend($.gritter.options, {
             fade_in_speed: 100, // how fast notifications fade in (string or int)
             fade_out_speed: 100, // how fast the notices fade out
-            time: 500 // hang on the screen for...
+            time: 1500 // hang on the screen for...
           });
 });
