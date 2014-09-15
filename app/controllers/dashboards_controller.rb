6835129c8c77a04d7bf1633bf39677e26d1b597a
grimmstede <grimmstede@feemon.com>
2014-09-14 21:38:37 -0500

    added project create

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,5 +3,9 @@ class DashboardsController < ApplicationController
   #before_action :require_admin
   before_action :get_user
   def index
+    if session[:user_id]
+      @user = current_user
+    end
+      @projects = Project.where(:user_id => @user.id).order(:name)
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,25 @@
 class ProjectsController < ApplicationController
+  before_action :get_user
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.user_id = @user.id
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to dashboards_path, notice: 'Project was successfully created.' }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def project_params
+      params.require(:project).permit(:name)
+  end
 end
