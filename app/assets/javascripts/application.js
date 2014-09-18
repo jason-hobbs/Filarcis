9914a537cf8ef75421f458dc843ae7f084c31640
grimmstede <grimmstede@feemon.com>
2014-09-17 21:10:34 -0500

    added jquery file upload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require foundation
 //= require turbolinks
 //= require gritter
+//= require jquery-fileupload
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :uploads
+
   resources :projects
 
   resources :users
