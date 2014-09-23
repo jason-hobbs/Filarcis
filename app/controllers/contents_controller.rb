83dbc93ef21831e40af35cbacfa646f16a8fe8e4
grimmstede <grimmstede@feemon.com>
2014-09-22 21:56:44 -0500

    content work

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,9 +2,30 @@ class ContentsController < ApplicationController
   before_action :require_signin
   #before_action :require_admin
   before_action :get_user
+  before_action :get_project
   def index
-    @project = Project.find(params[:project_id])
+
     @contents = @project.contents
 
   end
+
+  def new
+    @content=Content.new
+  end
+
+def create
+      @content = Content.new(content_params)
+      if @content.save
+        redirect_to dashboards_path, :gflash => { :success => "Files Uploaded" }
+      else
+        render :new
+      end
+end
+
+private
+
+def content_params
+    params.require(:content).permit(:location)
+end
+
 end
