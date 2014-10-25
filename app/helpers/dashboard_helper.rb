927b57722ce69e6242942c3d870a59afcf2325ac
Jason Hobbs <grimmstede@feemon.com>
2014-10-24 20:59:00 -0500

    ancestry work

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -7,7 +7,7 @@ module DashboardHelper
       #content_tag(:div, link_to folder.name, project_contents_path(folder), remote: true)
 
 
-      link_to("#{folder.name}<br>".html_safe, project_contents_path(folder), remote: true) + content_tag(:div, nested_folders(sub_folders), :class => "nested_folders")
+      link_to(image_tag("folder2.png", {:size=>"25" }), project_contents_path(folder), remote: true) + link_to("#{folder.name}<br>".html_safe, project_contents_path(folder), remote: true) + content_tag(:div, nested_folders(sub_folders), :class => "nested_folders")
 
 
 
