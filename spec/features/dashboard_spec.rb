4a328ee5b3f457b23de4ff7c273417d8043f97c3
Jason Hobbs <grimmstede@feemon.com>
2015-01-14 10:30:23 -0600

    added user auth to rspec tests

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -3,12 +3,15 @@ require "rails_helper"
 User.create(user_attributes)
 Project.create(project_attributes)
 
-describe "logging in" do
-  it "logs a user in" do
+describe "dashboard controller" do
+  before(:each) do
     visit '/session/new'
     fill_in 'email', :with => 'william.wallace@scotland.com'
     fill_in 'password', :with => 'Testing1'
     click_button 'Sign In'
+  end
+  it "shows dashboard" do
+    visit root_url
     expect(page).to have_content 'Inbox'
     expect(page).to have_content 'Personal'
     expect(page).to have_content 'Project Folders'
