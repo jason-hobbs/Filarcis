b67e1f5f8f42b04361cdeaed443bf668ecc78a5e
Jason Hobbs <grimmstede@feemon.com>
2015-01-24 23:27:19 -0600

    search work

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,11 @@
 class SearchController < ApplicationController
+  respond_to :html, :json
+  before_action :require_signin
+  before_action :get_user
+
+  def index
+    @query = params[:query]
+    @notes = Note.where('title ilike ?', "%#{@query}%")
+    @contents = Content.where('name ilike ?', "%#{@query}%")
+  end
 end
