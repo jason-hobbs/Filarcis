a7bb5a3eca6c57d68a887a689779874da24fe12e
Jason Hobbs <grimmstede@feemon.com>
2015-01-28 19:09:31 -0600

    search work

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,24 +1,28 @@
 module ContentsHelper
-  def file_type(content)
-     case content.content_type
-       when /^image/
-         image_tag(content.location.url(:thumb).to_s, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => "content_name")
-       when /pdf/
-         #image_tag("pdf3.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(@project, content)), :class => "content_name")
-         image_tag("pdf3.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => "content_name")
-       when /msword/
-         image_tag("doc.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => "content_name")
-       when /x-apple-diskimage/
-         image_tag("dmg.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => "content_name")
-       when /zip/
-         image_tag("zip.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => "content_name")
-       when /x-msdos-program/
-         image_tag("exe.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => "content_name")
-       when /officedocument.spreadsheetml.sheet/
-         image_tag("xls.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => "content_name")
-       else
-         image_tag("default2.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => "content_name")
-     end
+  def file_type(content, controller)
+    if controller == 'search'
+      classname = 'content_name_search'
+    else
+      classname = 'content_name'
+    end
+    case content.content_type
+      when /^image/
+        image_tag(content.location.url(:thumb).to_s, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
+      when /pdf/
+        image_tag("pdf3.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
+      when /msword/
+        image_tag("doc.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
+      when /x-apple-diskimage/
+        image_tag("dmg.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
+      when /zip/
+        image_tag("zip.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
+      when /x-msdos-program/
+        image_tag("exe.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
+      when /officedocument.spreadsheetml.sheet/
+        image_tag("xls.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
+      else
+        image_tag("default2.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
+    end
   end
 
   def file_image(content)
