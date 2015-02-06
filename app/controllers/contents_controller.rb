57ca59c17f5e7d8f7305553100c9b0b68ecc51f4
Jason Hobbs <grimmstede@feemon.com>
2015-02-05 21:48:05 -0600

    download file changes

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,10 +1,10 @@
 class ContentsController < ApplicationController
   respond_to :html, :json
-  before_action :require_signin
-  before_action :get_user
+  before_action :require_signin, except: [:download_file]
+  before_action :get_user, except: [:download_file]
   before_action :get_project
-  before_action :get_inbox
-  before_action :require_correct_user
+  before_action :get_inbox, except: [:download_file]
+  before_action :require_correct_user, except: [:download_file]
   before_action :get_content, only: [:show, :update]
 
   def index
