b357e13d2f2fa4055af913a1b76294b4729375c6
Jason Hobbs <grimmstede@feemon.com>
2015-02-08 12:30:16 -0600

    finished share

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -5,27 +5,29 @@ module ContentsHelper
     else
       classname = 'content_name'
     end
-    case content.content_type
-      when /^image/
-        image_tag(content.location.url(:thumb).to_s, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
-      when /pdf/
-        image_tag("pdf3.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
-      when /msword/
-        image_tag("doc.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
-      when /x-apple-diskimage/
-        image_tag("dmg.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
-      when /zip/
-        image_tag("zip.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
-      when /x-msdos-program/
-        image_tag("exe.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
-      when /officedocument.spreadsheetml.sheet/
-        image_tag("xls.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
-      else
-        image_tag("default2.png", :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
-    end
+    image = icon(content)
+    image_tag(image, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, link_to(content.name, project_content_path(content.project, content), data: {:"reveal-id" => "myModal", :"reveal-ajax" => true}), :class => classname)
   end
 
   def file_image(content)
+    image = icon(content)
+    if content.public
+      if image.include?('dmg') || image.include?('zip')  || image.include?('exe') || image.include?('default2') || image.include?('doc')
+        content_tag(:div, image_tag(image, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, best_in_place(content, :name, :url => project_content_path(@project,content), :class => "input_field"), :class => "file_name"), :class => "fileopen_row") + content_tag(:div, "Share link:") + text_field(:share, :url, :value => "#{request.host}#{content.location.url}", :readonly => "readonly", :size => 50) + link_to("Download File", download_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("Unshare", share_project_contents_path(:content => content.id), :remote => true, :class => 'button tiny radius space')
+      else
+        content_tag(:div, image_tag(image, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, best_in_place(content, :name, :url => project_content_path(@project,content), :class => "input_field"), :class => "file_name"), :class => "fileopen_row") + content_tag(:div, "Share link:") + text_field(:share, :url, :value => "#{request.host}#{content.location.url}", :readonly => "readonly", :size => 50) + link_to("Download File", download_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("View File", view_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("Unshare", share_project_contents_path(:content => content.id), :remote => true, :class => 'button tiny radius space')
+      end
+    else
+      if image.include?('dmg') || image.include?('zip') || image.include?('exe') || image.include?('default2') || image.include?('doc')
+        content_tag(:div, image_tag(image, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, best_in_place(content, :name, :url => project_content_path(@project,content), :class => "input_field"), :class => "file_name"), :class => "fileopen_row") + link_to("Download File", download_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("Share", share_project_contents_path(:content => content.id), :remote => true, :class => 'button tiny radius space')
+      else
+        content_tag(:div, image_tag(image, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, best_in_place(content, :name, :url => project_content_path(@project,content), :class => "input_field"), :class => "file_name"), :class => "fileopen_row") + link_to("Download File", download_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("View File", view_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("Share", share_project_contents_path(:content => content.id), :remote => true, :class => 'button tiny radius space')
+      end
+    end
+  end
+
+
+  def icon(content)
     case content.content_type
       when /^image/
         image = content.location.url(:thumb).to_s
@@ -43,19 +45,6 @@ module ContentsHelper
         image = "xls.png"
       else
         image = "default2.png"
-    end
-    if content.public
-      if image.include?('dmg') || image.include?('zip')  || image.include?('exe') || image.include?('default2')
-        content_tag(:div, image_tag(image, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, best_in_place(content, :name, :url => project_content_path(@project,content), :class => "input_field"), :class => "file_name"), :class => "fileopen_row") + content_tag(:div, "Share link:") + text_field(:share, :url, :value => "#{request.host}#{content.location.url}", :readonly => "readonly", :size => 50) + link_to("Download File", download_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("Unshare", share_project_contents_path(:content => content.id), :remote => true, :class => 'button tiny radius space')
-      else
-        content_tag(:div, image_tag(image, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, best_in_place(content, :name, :url => project_content_path(@project,content), :class => "input_field"), :class => "file_name"), :class => "fileopen_row") + content_tag(:div, "Share link:") + text_field(:share, :url, :value => "#{request.host}#{content.location.url}", :readonly => "readonly", :size => 50) + link_to("Download File", download_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("View File", view_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("Unshare", share_project_contents_path(:content => content.id), :remote => true, :class => 'button tiny radius space')
-      end
-    else
-      if image.include?('dmg') || image.include?('zip') || image.include?('exe') || image.include?('default2')
-        content_tag(:div, image_tag(image, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, best_in_place(content, :name, :url => project_content_path(@project,content), :class => "input_field"), :class => "file_name"), :class => "fileopen_row") + link_to("Download File", download_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("Share", share_project_contents_path(:content => content.id), :remote => true, :class => 'button tiny radius space')
-      else
-        content_tag(:div, image_tag(image, :style => 'float:left;', :class => "img_preview", :id => content.id) + content_tag(:div, best_in_place(content, :name, :url => project_content_path(@project,content), :class => "input_field"), :class => "file_name"), :class => "fileopen_row") + link_to("Download File", download_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("View File", view_file_project_contents_path(:content => content.id), :class => 'button tiny radius space', :target => "_blank") + link_to("Share", share_project_contents_path(:content => content.id), :remote => true, :class => 'button tiny radius space')
-      end
     end
   end
 end
