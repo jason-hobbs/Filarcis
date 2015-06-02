6a1cbcbe2af40882faa622849941c8979528534c
Jason Hobbs <grimmstede@feemon.com>
2015-06-02 14:04:30 -0500

    webhook work

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -2,8 +2,10 @@ class GithubWebhooksController < ActionController::Base
   include GithubWebhook::Processor
 
   def push(payload)
-    # TODO: handle push webhook
-    redirect_to root_path
+    respond_to do |format|
+      format.json { render json: "OK"}
+      format.html
+    end
   end
 
   def webhook_secret(payload)
