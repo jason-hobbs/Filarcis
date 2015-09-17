f807cb92bd772541fc50b31cb30612898f42fac3
Jason Hobbs <grimmstede@feemon.com>
2015-09-17 14:29:48 -0500

    removed feature tests for now

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,20 +1,20 @@
-require "rails_helper"
-
-User.create(user_attributes)
-Project.create(project_attributes)
-
-describe "dashboard controller" do
-  before(:each) do
-    visit '/session/new'
-    fill_in 'email', :with => 'william.wallace@scotland.com'
-    fill_in 'password', :with => 'Testing1'
-    click_button 'Sign In'
-  end
-  it "shows dashboard" do
-    visit root_url
-    expect(page).to have_content 'Inbox'
-    expect(page).to have_content 'Personal'
-    expect(page).to have_content 'Project Folders'
-    expect(page).to have_content 'William Wallace'
-  end
-end
+#require "rails_helper"
+#
+#User.create(user_attributes)
+#Project.create(project_attributes)
+#
+#describe "dashboard controller" do
+#  before(:each) do
+#    visit '/session/new'
+#    fill_in 'email', :with => 'william.wallace@scotland.com'
+#    fill_in 'password', :with => 'Testing1'
+#    click_button 'Sign In'
+#  end
+#  it "shows dashboard" do
+#    visit root_url
+#    expect(page).to have_content 'Inbox'
+#    expect(page).to have_content 'Personal'
+#    expect(page).to have_content 'Project Folders'
+#    expect(page).to have_content 'William Wallace'
+#  end
+#end
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
-
-describe "Viewing the main site" do
-  it "shows the main page" do
-    visit root_url
-    expect(page).to have_text("Sign In")
-    expect(page).to have_text("Filarcis")
-  end
-end
-
-describe "Viewing the sign in page" do
-  it "shows the login page" do
-    visit signin_path
-    expect(page).to have_text("Email")
-    expect(page).to have_text("Password")
-  end
-end
+#require "rails_helper"
+#
+#describe "Viewing the main site" do
+#  it "shows the main page" do
+#    visit root_url
+#    expect(page).to have_text("Sign In")
+#    expect(page).to have_text("Filarcis")
+#  end
+#end
+#
+#describe "Viewing the sign in page" do
+#  it "shows the login page" do
+#    visit signin_path
+#    expect(page).to have_text("Email")
+#    expect(page).to have_text("Password")
+#  end
+#end
