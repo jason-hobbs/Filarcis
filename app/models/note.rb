3e2f169b24e45f23fb2986d234951fb57fe3106a
Jason Hobbs <grimmstede@feemon.com>
2016-01-06 13:28:47 -0600

    expanded note content to 50000 long

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,5 +2,5 @@ class Note < ActiveRecord::Base
   belongs_to :project
   validates :title, presence: true
   validates :project_id, presence: true
-  validates_length_of :content, :in => 2..10000, :allow_blank => false
+  validates_length_of :content, :in => 2..50000, :allow_blank => false
 end
